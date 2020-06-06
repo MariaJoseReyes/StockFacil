448003aa8cb91c31cc25b989a19291ecbade1dcc
MariaJoseReyes <64438037+MariaJoseReyes@users.noreply.github.com>
2020-06-06 13:01:04 -0500

    Add files via upload

---
+++ StockFacil/src/main/java/Indice.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Indice { //desde donde empezara lo que se quiera hacer.
+
+    public void main (String [] args){
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("Bienvenido al organizador de stock");
+        System.out.println("Ingrese la opcion que le acomode");
+        System.out.println("[1] Nuevo objeto para stock");
+
+        NuevoStock nuevo = new NuevoStock();
+
+    }
+
+
+}
---
+++ StockFacil/src/main/java/NuevoStock.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class NuevoStock {
+
+    public void main (String []args){
+        Scanner leer = new Scanner(System.in);
+
+        String nuevoStock=leer.nextLine();
+        int cantidad=leer.nextInt();
+
+
+
+    }
+}
+
+
